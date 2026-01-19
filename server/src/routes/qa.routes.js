25b4669b6f6cd65e441a5dbfd720e4f4e355e241
JamaAX <jeremyalxirsi@gmail.com>
2026-01-19 23:28:41 +0300

    initial commit

--- server/src/routes/qa.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { listQA, createQA } = require("../controllers/qa.controller");
-
-const router = express.Router();
-
-router.get("/", listQA);
-router.post("/", createQA);
-
-module.exports = router;
